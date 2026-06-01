ed32d01f442fda6f4d72741c7bb3e4eb9f6c35fb
Ju Braz <tracocult@gmail.com>
2026-06-01 00:01:21 +0000

    kkkk

---
+++ src/test/java/com/TracoCultural/TracoCultural/TracoCulturalApplication.java
@@ -0,0 +1,13 @@
+package com.TracoCultural.TracoCultural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracoCulturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracoCulturalApplication.class, args);
+	}
+
+}
